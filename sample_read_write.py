bb190ad8ed6dbb4ccc931e246c7f5ab7186b751b
Rupesh Raj Karn <rupesh.karn@ku.ac.ae>
2022-06-22 10:34:35 +0400

    EXP Sample read write

--- sample_read_write.py
+++ sample_read_write.py
@@ -1,7 +1,13 @@
-f = open('sample01.txt', 'w')
-f.write('Hello World')
+import os
+from pathlib import Path
+file_path_write = Path(os.environ["ICHOR_OUTPUT_DATASET"]) / 'sample_write.txt'
+file_path_read = Path(os.environ["ICHOR_INPUT_DATASET"]) / 'sample01.txt'
+
+f = open(file_path_write, 'w')
+f.write('Hello World. I have written succesfully.')
 f.close
 
-f = open('sample01.txt', 'r')
+f = open(file_path_read, 'r')
 file_content = f.readlines()
 print(file_content)
+f.close()

043d9212604a3a080f08ce39858a65bccc0a5b7a
Rupesh Raj Karn <rupesh.karn@ku.ac.ae>
2022-06-23 12:38:57 +0000

    EXP malnet processing

--- malnet_processing.py
+++ malnet_processing.py
@@ -5,3 +5,27 @@ file_path_read = Path(os.environ["ICHOR_INPUT_DATASET"]) / "malnet-images-tiny"
 print('The details of the train directory files.',os.listdir(file_path_read/"train"))
 print('The details of the test directory files.',os.listdir(file_path_read/"test"))
 print('The details of the val directory files.',os.listdir(file_path_read/"val"))
+
+
+train_dir = os.listdir(file_path_read/"train")
+test_dir = os.listdir(file_path_read/"test")
+val_dir = os.listdir(file_path_read/"val")
+X_train = []
+Y_train_fine = []
+Y_train_coarse = []
+for dirs in train_dir:
+    for sub_class in os.listdir(train_dir/dirs):
+        for image_file in os.listdir(train_dir/dirs/sub_class):
+            im = Image.open(train_dir/dirs/image_file, 'r')
+            print(f'Processing for dir {dirs}, subdir {sub_class}, file {image_file}.')
+            X_train.append(np.asarray(im))
+            Y_train_fine.append(sub_class)
+            Y_train_coarse.append(dirs)
+            
+import pickle
+train_dataset = {}
+train_dataset['X_train'] = X_train
+train_dataset['Y_train_fine'] = Y_train_fine
+train_dataset['Y_train_coarse'] = Y_train_coarse
+with open(file_path_read/"malnet_train", "w+") as fp:  
+    pickle.dump(train_dataset, fp)        

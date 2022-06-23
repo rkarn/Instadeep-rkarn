f80c3b70eaa7eab7bfb870cb82c5501e4a170c6e
Rupesh Raj Karn <rupesh.karn@ku.ac.ae>
2022-06-23 12:03:19 +0000

    EXP malnet processing

--- malnet_processing.py
+++ malnet_processing.py
@@ -2,4 +2,4 @@ import os
 from pathlib import Path
 
 file_path_read = Path(os.environ["ICHOR_INPUT_DATASET"]) / "malnet-images-tiny"
-print('The details of the current directory files.',os.listdir())
+print('The details of the current directory files.',os.listdir(file_path_read))

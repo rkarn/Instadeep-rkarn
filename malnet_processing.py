5ae77e0350470d7e16a1b6ab0675e24eec01ab86
Rupesh Raj Karn <rupesh.karn@ku.ac.ae>
2022-06-24 06:29:43 +0000

    EXP malnet processing

--- malnet_processing.py
+++ malnet_processing.py
@@ -8,16 +8,16 @@ print('The details of the train directory files.',os.listdir(file_path_read/"tra
 print('The details of the test directory files.',os.listdir(file_path_read/"test"))
 print('The details of the val directory files.',os.listdir(file_path_read/"val"))
 
-train_dir = os.listdir(file_path_read/"train")
-test_dir = os.listdir(file_path_read/"test")
-val_dir = os.listdir(file_path_read/"val")
+train_dir = Path(os.environ["ICHOR_INPUT_DATASET"]) / "malnet-images-tiny" / "train"
+test_dir = Path(os.environ["ICHOR_INPUT_DATASET"]) / "malnet-images-tiny" / "test"
+val_dir = Path(os.environ["ICHOR_INPUT_DATASET"]) / "malnet-images-tiny" / "val"
 X_train = []
 Y_train_fine = []
 Y_train_coarse = []
 for dirs in train_dir:
-    for sub_class in os.listdir(Path(os.environ["ICHOR_INPUT_DATASET"]) / "malnet-images-tiny" / dirs):
-        for image_file in os.listdir(Path(os.environ["ICHOR_INPUT_DATASET"]) / "malnet-images-tiny" / dirs / sub_class):
-            im = Image.open(Path(os.environ["ICHOR_INPUT_DATASET"]) / "malnet-images-tiny" / dirs / sub_class / image_file, 'r')
+    for sub_class in os.listdir(train_dir / dirs):
+        for image_file in os.listdir(train_dir / dirs / sub_class):
+            im = Image.open(train_dir / dirs / sub_class / image_file, 'r')
             print(f'Processing for dir {dirs}, subdir {sub_class}, file {image_file}.')
             X_train.append(np.asarray(im))
             Y_train_fine.append(sub_class)

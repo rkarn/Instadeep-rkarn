9090e5195755368646a1991123b76f7b2286904b
Rupesh Raj Karn <rupesh.karn@ku.ac.ae>
2022-06-27 11:35:53 +0000

    EXP sample read write

--- sample_read_write.py
+++ sample_read_write.py
@@ -1,13 +1,13 @@
 import os
 from pathlib import Path
 
-file_path_read = Path(os.environ["ICHOR_INPUT_DATASET"]) / 'malnet-images-tiny' / 'sample01.txt'
+file_path_read = Path(os.environ["ICHOR_INPUT_DATASET"]) / 'sample01.txt'
 f = open(file_path_read, 'r')
 file_content = f.readlines()
 print(file_content)
 f.close()
 
-file_path_write = Path(os.environ["ICHOR_INPUT_DATASET"]) / 'malnet-images-tiny' / 'sample_write.txt'
+file_path_write = Path(os.environ["ICHOR_INPUT_DATASET"]) / 'sample_write.txt'
 f = open(file_path_write, 'w+')
 f.write('Hello World. I have written succesfully.')
 f.close()

34e64369ff21d9583ff12c600b6acf7afc8ad20c
Rupesh Raj Karn <rupesh.karn@ku.ac.ae>
2022-07-04 17:01:04 +0400

    ml classifier

---
+++ ml_train_test.py
@@ -0,0 +1,17 @@
+import os
+import pickle
+
+train_data_file = Path(os.environ["ICHOR_INPUT_DATASET"]) / 'malnet-images-tiny' / 'malnet_train'
+train_data = pickle.load(train_data_file)
+X_train = train_data['X_train']
+Y_train_fine = train_data['Y_train_fine']
+Y_train_coarse = train_data['Y_train_coarse']
+
+test_data_file = Path(os.environ["ICHOR_INPUT_DATASET"]) / 'malnet-images-tiny' / 'malnet_test'
+test_data = pickle.load(test_data_file)
+X_test = train_data['X_test']
+Y_test_fine = train_data['Y_test_fine']
+Y_test_coarse = train_data['Y_test_coarse']
+
+print('Train set', X_train.shape, Y_train_fine.shape, Y_train_coarse.shape)
+print('Test set', X_test.shape, Y_test_fine.shape, Y_test_coarse.shape)

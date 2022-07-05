ff22b5acaf30e8b8331d70d06d33f4445848763c
Rupesh Raj Karn <rupesh.karn@ku.ac.ae>
2022-07-05 10:06:48 +0400

    EXP ML classification

--- ml_train_test.py
+++ ml_train_test.py
@@ -3,13 +3,17 @@ from pathlib import Path
 import pickle
 
 train_data_file = Path(os.environ["ICHOR_INPUT_DATASET"]) / 'malnet-images-tiny' / 'malnet_train'
-train_data = pickle.load(train_data_file)
+with open(train_data_file, 'rb') as pickle_file_train:
+    train_data = pickle.load(pickle_file_train)
+print('Train pickle file loaded.')
 X_train = train_data['X_train']
 Y_train_fine = train_data['Y_train_fine']
 Y_train_coarse = train_data['Y_train_coarse']
 
 test_data_file = Path(os.environ["ICHOR_INPUT_DATASET"]) / 'malnet-images-tiny' / 'malnet_test'
-test_data = pickle.load(test_data_file)
+with open(test_data_file, 'rb') as pickle_file_test:
+    test_data = pickle.load(pickle_file_test)
+print('Test pickle file loaded.')
 X_test = train_data['X_test']
 Y_test_fine = train_data['Y_test_fine']
 Y_test_coarse = train_data['Y_test_coarse']
